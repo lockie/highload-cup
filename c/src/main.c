95d41b3050510bc4fbfae56dca574a3e88823714
Andrew Kravchuk <awkravchuk@gmail.com>
2017-08-29 19:43:53 +0300

    Added multi-threading

--- c/src/main.c
+++ c/src/main.c
@@ -3,10 +3,14 @@
 #include <inttypes.h>
 #include <string.h>
 #include <signal.h>
+#include <fcntl.h>
 #include <sys/mman.h>
 #include <netinet/tcp.h>
 
+#include <pthread.h>
+
 #include <event2/event.h>
+#include <event2/thread.h>
 
 #include <cJSON.h>
 
@@ -118,6 +122,84 @@ static void setup_memory(size_t pool_size)
     event_set_mem_functions(sqlite_malloc_fn, memsys5_realloc, sqlite_mm.xFree);
 }
 
+static int setup_socket(int portnum)
+{
+    int fd;
+    VERIFY_POSITIVE(fd = socket(AF_INET, SOCK_STREAM, 0));
+
+    int yes = 1;
+    VERIFY_ZERO(setsockopt(fd, SOL_SOCKET, SO_REUSEADDR,
+                           &yes, sizeof(int)));
+    VERIFY_ZERO(setsockopt(fd, IPPROTO_TCP, TCP_NODELAY,
+                           &yes, sizeof(int)));
+    VERIFY_ZERO(setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE,
+                           &yes, sizeof(int))<0)
+
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(portnum);
+
+    VERIFY_POSITIVE(bind(fd, (struct sockaddr*)&addr, sizeof(addr)));
+    VERIFY_POSITIVE(listen(fd, 2048));
+
+    int flags;
+    if ((flags = fcntl(fd, F_GETFL, 0)) < 0
+        || fcntl(fd, F_SETFL, flags | O_NONBLOCK) < 0)
+        return -1;
+
+    return fd;
+}
+
+void* thread_dispatch(void* arg)
+{
+    event_base_dispatch((struct event_base*)arg);
+    return NULL;
+}
+
+static void setup_threads(int count, int portnum, database_t* database)
+{
+    VERIFY_ZERO(evthread_use_pthreads());
+
+    int fd = setup_socket(portnum);
+    pthread_t* threads = alloca(count * sizeof(pthread_t));
+    const char* method = NULL;
+    for(int i = 0; i < count; i++)
+    {
+        struct event_config* config;
+        VERIFY_NONZERO(config = event_config_new());
+        if(count == 1)
+            event_config_set_flag(config, EVENT_BASE_FLAG_NOLOCK);
+        event_config_set_flag(config, EVENT_BASE_FLAG_EPOLL_USE_CHANGELIST);
+
+        struct event_base* base;
+        VERIFY_NONZERO(base = event_base_new_with_config(config));
+        event_config_free(config);
+
+        if(verbose && i == 0)
+            method = event_base_get_method(base);
+
+        struct evhttp* httpd;
+        VERIFY_NONZERO(httpd = evhttp_new(base));
+        evhttp_set_default_content_type(httpd, "application/json; charset=utf-8");
+        evhttp_set_allowed_methods(httpd, EVHTTP_REQ_GET | EVHTTP_REQ_POST);
+        evhttp_set_gencb(httpd, request_handler, database);
+        VERIFY_ZERO(evhttp_accept_socket(httpd, fd));
+
+        VERIFY_ZERO(pthread_create(&threads[i], NULL, thread_dispatch, base));
+    }
+
+    if(verbose)
+        printf("Listening on port %d using %s in %d thread(s)\n",
+               portnum, method, count);
+
+    for(int i = 0; i < count; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+}
+
 int main(int argc, char** argv)
 {
     // Docker <_<
@@ -140,10 +222,20 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     }
 #endif
+    if(args.threads_arg != 1 && args.phase_hack_flag)
+    {
+        fprintf(stderr, "Can't do phase hack in multithreaded mode!\n");
+        return EXIT_FAILURE;
+    }
 
     setup_signals();
 
     setup_memory(1L << args.memory_arg);
+
+    if(args.threads_arg == 1)
+    {
+        VERIFY_ZERO(sqlite3_config(SQLITE_CONFIG_SINGLETHREAD));
+    }
 
     database_t database;
     MEASURE_DURATION(VERIFY_ZERO(bootstrap(&database, args.data_arg)),
@@ -160,28 +252,7 @@ int main(int argc, char** argv)
     }
 #endif  /* NDEBUG */
 
-    struct event_config* config;
-    VERIFY_NONZERO(config = event_config_new());
-    event_config_set_flag(config, EVENT_BASE_FLAG_NOLOCK);
-    event_config_set_flag(config, EVENT_BASE_FLAG_EPOLL_USE_CHANGELIST);
-    struct event_base* base;
-    VERIFY_NONZERO(base = event_base_new_with_config(config));
-    event_config_free(config);
-    struct evhttp* http;
-    VERIFY_NONZERO(http = evhttp_new(base));
-    evhttp_set_default_content_type(http, "application/json; charset=utf-8");
-    evhttp_set_allowed_methods(http, EVHTTP_REQ_GET | EVHTTP_REQ_POST);
-    evhttp_set_gencb(http, request_handler, &database);
-    struct evhttp_bound_socket* socket;
-    VERIFY_NONZERO(socket = evhttp_bind_socket_with_handle(
-                       http, "0.0.0.0", args.port_arg));
-    int yes = 1;
-    VERIFY_ZERO(setsockopt(evhttp_bound_socket_get_fd(socket),
-                           IPPROTO_TCP, TCP_NODELAY, &yes, sizeof(int)));
-    if(verbose)
-        printf("Listening on port %d using %s\n", args.port_arg,
-               event_base_get_method(base));
-    event_base_dispatch(base);
+    setup_threads(args.threads_arg, args.port_arg, &database);
 
     return EXIT_SUCCESS;
 }
